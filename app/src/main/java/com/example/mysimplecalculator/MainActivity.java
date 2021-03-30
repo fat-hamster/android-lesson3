31a350680a2a621f70798f36560a04d0ada091a9
Dmitriy Gridchin <tigris.tstu@gmail.com>
2021-03-30 09:19:14 +0300

    Initial lesson5 homework

--- app/src/main/java/com/example/mysimplecalculator/MainActivity.java
+++ app/src/main/java/com/example/mysimplecalculator/MainActivity.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
